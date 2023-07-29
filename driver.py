f19f54b3f930d38219b5f3dd5a97a2c0a99a975b
Naomi Meininger <119152187+naomi-meininger@users.noreply.github.com>
2023-07-29 01:15:53 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -16,4 +16,5 @@ def get_info():
   return jsonify(scraping.scrapeData(search_param))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
